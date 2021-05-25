d0e62e61bc3cf2c442569e2cfbe731a8ca5f9117
Simon Elvery <simon@elvery.net>
2021-05-25 16:33:01 +1000

    Remove a console.log 🧹

--- src/routes/stage.svelte
+++ src/routes/stage.svelte
@@ -2,7 +2,6 @@
   import StageLayer from '$lib/StageLayer.svelte';
   import { progress, options } from '$lib/storage';
   import { layers } from '$lib/storage';
-  console.log('$layers :>> ', $layers);
 </script>
 
 {#each $layers.slice().reverse() as layer (layer.id)}
