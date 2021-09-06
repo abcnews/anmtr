ddeadc498027eda2b2da4a6f390f764af08c4604
Simon Elvery <simon@elvery.net>
2021-09-06 11:58:55 +1000

    Remove unused route

--- src/routes/stage.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import StageLayer from '$lib/StageLayer.svelte';
-  import { progress, options } from '$lib/storage';
-  import { layers } from '$lib/storage';
-</script>
-
-{#each $layers.slice().reverse() as layer (layer.id)}
-  <StageLayer {layer} stageWidth={$options.width} stageHeight={$options.height} />
-{/each}
